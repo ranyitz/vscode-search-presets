b203e4b52ca9769b99d8e1ed0638fd183df3e4c4
Ran Yitzhaki <ranyitz@gmail.com>
2022-09-16 02:05:16 +0200

    initial poc

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,59 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+// let success = await vscode.commands.executeCommand('search.action.openEditor', searchConfig);
+// type SearchConfiguration = {
+// 	query: string,
+// 	includes: string,
+// 	excludes: string,
+// 	contextLines: number,
+// 	wholeWord: boolean,
+// 	caseSensitive: boolean,
+// 	regexp: boolean,
+// 	useIgnores: boolean,
+// 	showIncludesExcludes: boolean,
+// };
+
+interface IFindInFilesArgs {
+	query?: string;
+	replace?: string;
+	triggerSearch?: boolean;
+	filesToInclude?: string;
+	filesToExclude?: string;
+	isRegex?: boolean;
+	isCaseSensitive?: boolean;
+	matchWholeWord?: boolean;
+}
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "search-presets" is now active!');
+	let disposable = vscode.commands.registerCommand('search-presets.search', async () => {
+		const workbenchConfig = vscode.workspace.getConfiguration();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('search-presets.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from search-presets!');
+		const presets = workbenchConfig.get('search-presets');
+		// TODO: validate presets
+		
+		// TODO: if there is an existing query - reuse it!
+		const searchConfig: Partial<IFindInFilesArgs> = {
+			query: '',
+			triggerSearch: false,
+			filesToExclude: '',
+			filesToInclude: '',
+		};
+
+		const presetNames = Object.keys(presets as Object);
+
+		vscode.window.showQuickPick(presetNames).then(selectedPresetName => {
+			// the user canceled the selection
+			if (!selectedPresetName) {
+			  return;
+			}
+		  
+			const selectedPreset = (presets as any)[selectedPresetName];
+
+			vscode.commands.executeCommand('workbench.action.findInFiles', {...searchConfig, ...selectedPreset});
+		  });
 	});
 
 	context.subscriptions.push(disposable);
