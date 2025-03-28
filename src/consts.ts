2902d87c6b295079cabcd2000a5199de7ea9fc95
Ran Yitzhaki <ranyitz@gmail.com>
2025-03-28 23:42:52 +0100

    feat: add a ui for creating presets via a command

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 export const NO_CONFIG_KEY_WARNING = "hasShownNoConfigWarning";
 export const CONFIG_SECTION_KEY = "search-presets";
 export const CUSTOM_PRESETS_CONFIG_KEY = "custom";
-export const COMMAND_KEY = "search-presets.search";
+export const SEARCH_COMMAND_KEY = "search-presets.search";
+export const CREATE_PRESET_COMMAND_KEY = "search-presets.createPreset";
 export const FIND_IN_FILES_COMMAND_KEY = "workbench.action.findInFiles";
